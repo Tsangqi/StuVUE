2152a6ae871c644d85256c52cf1289783e56c9ff
Tsangqi <1074838955@qq.com>
2020-04-03 22:35:10 +0800

    第一天（学习搭建vue）

---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '你好呀，曾琦！哈哈哈哈！'
+    }
+  }
+}
+</script>
